f727260268c043d2fd0603d1885f33924b7d95ca
Max <Max@145.89.157.175>
2012-06-13 10:59:43 +0200

    added alternative loginTask

---
+++ src/mediaApp/HTTP/ProxyLoginTaskAlt.java
@@ -0,0 +1,84 @@
+package mediaApp.HTTP;
+
+import java.net.SocketTimeoutException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class ProxyLoginTaskAlt extends AsyncTask<String, Void, String>
+{
+	static final String TAG = "ProxyLoginTask2";
+	private final HTTPResponseListener	listener;
+
+	public ProxyLoginTaskAlt(HTTPResponseListener list)
+	{
+		listener = list;
+	}
+
+	/**
+	 * Logs into the proxy server
+	 * 
+	 * @param args
+	 *            Needs an array of strings that is 3 strings in size. The first string should be the URL of
+	 *            the proxy where we can log in. The second string should be the username (email) and the
+	 *            third string should be the password.
+	 * @return Returns the response from the proxy to the {@link onResponseReceived} function of the listener
+	 *         specified in the constructor.
+	 */
+	@Override
+	protected String doInBackground(String... args)
+	{
+		HttpResponse response = null;
+		
+		try
+		{
+			URL url = new URL(args[0]);
+
+			HttpClient httpclient = new DefaultHttpClient();	    
+		    HttpPost httppost = new HttpPost(args[0]);
+
+		    List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+	        nameValuePairs.add(new BasicNameValuePair("user", args[1]));
+	        nameValuePairs.add(new BasicNameValuePair("pass", args[2]));
+	        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+	        // Execute HTTP Post Request
+	        response = httpclient.execute(httppost);
+	        
+	        Log.i(TAG, EntityUtils.toString(response.getEntity()));
+		}
+		catch (SocketTimeoutException e)
+		{
+			// Log.e(TAG, "doInBackground socket timed out", e);
+		}
+		catch (Exception e)
+		{
+			// Log.e(TAG, "Error in doInbackground: ", e);
+		}
+		Log.i("proxy","done");
+		return response.getEntity().toString();
+	}
+
+	@Override
+	protected void onPostExecute(String result)
+	{
+		Log.i("proxy","onPostExecute");
+		if (listener != null && result != null)
+		{
+			listener.onResponseReceived(result);
+		}
+	}
+
+}
