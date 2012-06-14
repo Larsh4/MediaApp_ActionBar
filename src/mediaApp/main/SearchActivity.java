a0173d66c0a9678aa5eebc2ac627b0d07d22c704
Max <Max@145.89.157.175>
2012-06-14 15:13:16 +0200

    nothing really

--- src/mediaApp/main/SearchActivity.java
+++ src/mediaApp/main/SearchActivity.java
@@ -2,9 +2,12 @@ package mediaApp.main;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import mediaApp.HTTP.HTTPGetTaskAlt;
 import mediaApp.HTTP.HTTPResponseListener;
+
 import org.apache.http.NameValuePair;
+
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.ProgressDialog;
