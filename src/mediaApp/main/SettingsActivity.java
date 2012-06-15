e0b136384f595c10a4a4da68eef38640e54a5611
Larsh4 <lars.h4@gmail.com>
2012-06-15 11:32:30 +0200

    Fixed settings menu Added logout button to settings Fixed MainActivity filling textfields with data

--- src/mediaApp/main/SettingsActivity.java
+++ src/mediaApp/main/SettingsActivity.java
@@ -1,27 +1,36 @@
 package mediaApp.main;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
 
-public class SettingsActivity extends BaseActivity implements 
-			OnCheckedChangeListener, android.widget.CompoundButton.OnCheckedChangeListener
+public class SettingsActivity extends BaseActivity implements
+		OnCheckedChangeListener,
+		android.widget.CompoundButton.OnCheckedChangeListener,
+		OnClickListener
 {
-	static final String TAG 			= "SettingsAct";
-	
-	//UI
+
+	static final String	TAG				= "SettingsAct";
+
+	// UI
 	RadioGroup			RGStartup;
 	CheckBox			CBShowSearch;
-	//Preference Keys
-	static final String	SELECTION_KEY	= "RGStartupSel";	
+	// Preference Keys
+	static final String	SELECTION_KEY	= "RGStartupSel";
 	static final String	SHOW_KEY		= "CHShowSearch";
+	private Button		logoutButton;
 
 	public void onCreate(Bundle savedInstanceState)
 	{
@@ -37,6 +46,26 @@ public class SettingsActivity extends BaseActivity implements
 		LoadPreferences();
 		RGStartup.setOnCheckedChangeListener(this);
 		CBShowSearch.setOnCheckedChangeListener(this);
+
+		logoutButton = (Button) findViewById(R.id.BLogout);
+		logoutButton.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v)
+	{
+		if (v == logoutButton)
+		{
+			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+			Editor editor = prefs.edit();
+			editor.remove(MainActivity.USER_KEY);
+			editor.remove(MainActivity.PASS_KEY);
+			editor.remove(MainActivity.REMEMBER_KEY);
+			editor.commit();
+
+			startActivity(new Intent(this, MainActivity.class));
+			finish();
+		}
 	}
 
 	@Override
@@ -44,7 +73,7 @@ public class SettingsActivity extends BaseActivity implements
 	{
 		MenuInflater menuInflater = getMenuInflater();
 		menuInflater.inflate(R.menu.settings, menu);
-		return true;//super.onCreateOptionsMenu(menu);
+		return true;// super.onCreateOptionsMenu(menu);
 	}
 
 	private void LoadPreferences()
@@ -53,7 +82,8 @@ public class SettingsActivity extends BaseActivity implements
 		int RadioButtonSelection = sharedPreferences.getInt(SELECTION_KEY, R.id.RBNews);
 		RGStartup.check(RadioButtonSelection);
 		int CheckBoxState = sharedPreferences.getInt(SHOW_KEY, 0);
-		if(CheckBoxState==1)	CBShowSearch.setChecked(true);
+		if (CheckBoxState == 1)
+			CBShowSearch.setChecked(true);
 	}
 
 	@Override
@@ -63,10 +93,13 @@ public class SettingsActivity extends BaseActivity implements
 	}
 
 	@Override
-	public void onCheckedChanged(CompoundButton arg0, boolean arg1) {
+	public void onCheckedChanged(CompoundButton arg0, boolean arg1)
+	{
 		int i;
-		if(arg1)i=1;
-		else 	i=0;
+		if (arg1)
+			i = 1;
+		else
+			i = 0;
 		SaveIntPreferences(SHOW_KEY, i);
 	}
 }
